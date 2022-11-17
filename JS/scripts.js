6482db7a48293be53b88c3a7a6dba05a4aa65116
k-costa <k.accosta98@hotmail.com>
2022-11-17 14:06:06 -0400

    Adicionando JQuery e deixando a Nav responsiva

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+    var btnMenuMobile = $('.btn-menumobile')
+
+    $(btnMenuMobile).on("click", function(){
+        $('.nav-container ul').toggleClass('open');
+    });
+
+});
